5eef82c972e02f4cbe9c0a00f72d14e66dcc1029
kris <hithard5@gmail.com>
2022-01-20 19:52:02 +0100

    Add player tests: create human and ai players

--- source/components/player/Player.js
+++ source/components/player/Player.js
@@ -1,3 +1,5 @@
-function createPlayer() {}
+function createPlayer(name, config = { isABot: false }) {
+  return { name, isABot: config.isABot };
+}
 
 export default createPlayer;
--- source/tests/Player.test.js
+++ source/tests/Player.test.js
@@ -1,6 +1,13 @@
 import { createPlayer } from '../components';
 
-test.skip('Create a player named Bob', () => {
+test('Create a player named Bob', () => {
   const player = createPlayer('Bob');
   expect(player.name).toBe('Bob');
+  expect(player.isABot).toBe(false);
+});
+
+test('Create an AI player', () => {
+  const aiPlayer = createPlayer('T-800', { isABot: true });
+  expect(aiPlayer.name).toBe('T-800');
+  expect(aiPlayer.isABot).toBe(true);
 });
