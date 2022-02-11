f46c7bc538214672b9a6af071e72349a204820f8
kris <hithard5@gmail.com>
2022-02-11 15:10:29 +0100

    Add Sweet Alerts 2 library

--- source/index.js
+++ source/index.js
@@ -3,14 +3,7 @@ import { game } from './components';
 import './css/reset.css';
 import './css/style.scss';
 
-// 1. Render starting screen with name input and a start button.
 game.initialize();
 
-/*
-
-Carrier	    5
-Battleship	4
-Destroyer	  3
-Submarine	  3
-Patrol Boat	2
-*/
+// SWAL TEST
+Swal.fire('The Internet?', 'That thing is still around?', 'question');
--- webpack.common.js
+++ webpack.common.js
@@ -31,7 +31,7 @@ module.exports = {
     new MiniCssExtractPlugin(),
 
     new webpack.ProvidePlugin({
-      _: 'lodash',
+      Swal: 'sweetalert2',
     }),
   ],
 
